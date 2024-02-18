422872d2c443274dd7df58d028b904299731f881
Sahan2000 <udanasahan1@gmail.com>
2024-02-18 14:26:53 +0530

    init the project

---
+++ src/components/TopNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TopNav = ()=>{
+    return(
+        <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
+
+        </div>
+    )
+}
+export default TopNav
