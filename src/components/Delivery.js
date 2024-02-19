6c8849016fd59e5502b26434c44fecdd8276bee7
Sahan2000 <udanasahan1@gmail.com>
2024-02-19 12:44:43 +0530

    add few changes

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -4,7 +4,7 @@ const Delivery = ()=> {
     return(
         <div className="w-full bg-white py-16 px-4">
             <h3 className="text-orange-500 font-bold text-2xl text-center">Quick Delivery App</h3>
-            <div className="w-[1240px] mx-auto grid md:grid-cols-2">
+            <div className="max-w-[1640px] mx-auto grid md:grid-cols-2 sm:max-w-7xl">
                 <img className="w-[550px] mx-auto my-4" src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp" alt=""/>
                 <div className="flex flex-col justify-center">
                     <p className="text-[#00df9a] font-bold"> Get The App</p>
--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {mealData} from "../data/data.js";
 import {ArrowSmRightIcon} from '@heroicons/react/outline'
 
 const Meals = ()=>{
+    const [foods,setFood] = useState(mealData);
+    const filterCategory = (category) =>{
+        setFood(
+            mealData.filter((item)=>{
+                return item.category === category
+            })
+        );
+    }
     return(
         <div className="max-w-[1920px] m-auto px-4 py-12">
             <h1 className="text-orange-500 font-bold text-2xl text-center py-2">Our Meal</h1>
             <div className="flex flex-col lg:flex-row justify-center">
                 <div className="flex justify-center md:justify-center">
-                    <button>All</button>
-                    <button>Pizza</button>
-                    <button>Chicken</button>
-                    <button>Salad</button>
+                    <button onClick={()=>setFood(mealData)} className="m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700 hover:border-orange-700">All</button>
+                    <button onClick={()=>filterCategory("pizza")} className="m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700 hover:border-orange-700">Pizza</button>
+                    <button onClick={()=>filterCategory("chicken")} className="m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700 hover:border-orange-700">Chicken</button>
+                    <button onClick={()=>filterCategory("salad")} className="m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700 hover:border-orange-700">Salad</button>
                 </div>
             </div>
             <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-6">
                 {
-                    mealData.map((item)=>(
+                    foods.map((item)=>(
                         <div key={item.id} className="border-none hover:scale-105 duration-300">
                             <img src={item.image} className="w-full h-[200px] object-cover rounded-lg" alt={item.name}/>
                             <div className="flex justify-between py-4 px-2">
